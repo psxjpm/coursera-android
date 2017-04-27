8913bf2b5d55fa86a722022441e2de7945808350
psxjpm <psxjpm@nottingham.ac.uk>
2017-04-27 10:05:04 -0600

    comments added

--- Examples/BcastRecSinBcastDynReg/src/course/examples/broadcastreceiver/singlebroadcastdynamicregistration/SingleBroadcast.java
+++ Examples/BcastRecSinBcastDynReg/src/course/examples/broadcastreceiver/singlebroadcastdynamicregistration/SingleBroadcast.java
@@ -11,8 +11,13 @@ import android.widget.Button;
 
 public class SingleBroadcast extends Activity {
 
+	//Intent Action String
 	private static final String CUSTOM_INTENT = "course.examples.BroadcastReceiver.show_toast";
+	
+	//Intent filter object passing action string
 	private final IntentFilter intentFilter = new IntentFilter(CUSTOM_INTENT);
+	
+	//BroadcastReceiver instance
 	private final Receiver receiver = new Receiver();
 
 	private LocalBroadcastManager mBroadcastMgr;
@@ -21,8 +26,11 @@ public class SingleBroadcast extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		//receiving intents WITHIN application (Dentro del mismo contexto)
 		mBroadcastMgr = LocalBroadcastManager
 				.getInstance(getApplicationContext());
+		
+		//PASO 1: Register receiver
 		mBroadcastMgr.registerReceiver(receiver, intentFilter);
 
 		setContentView(R.layout.main);
@@ -31,6 +39,8 @@ public class SingleBroadcast extends Activity {
 		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				
+				//Send Broadcast
 				mBroadcastMgr.sendBroadcast(new Intent(CUSTOM_INTENT));
 			}
 		});
